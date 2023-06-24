c25a4e29656bf3c7ba34912244820d8d39be9839
Evan de la Cruz <evan.delacruz@gmail.com>
2023-06-23 21:44:50 -0700

    change truffle config to use shanghai and solc 18

--- truffle-config.js
+++ truffle-config.js
@@ -87,14 +87,14 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.20",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.18",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       settings: {          // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
           enabled: true,
           runs: 10
         },
-        evmVersion: "shanghai"
+        evmVersion: "paris"
       }
     }
   },
